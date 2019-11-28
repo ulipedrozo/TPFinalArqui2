0aabfb1c2107386b6bd260339bb3bcde7c451c6c
Joacocattaneo <joacocattaneo@gmail.com>
2019-11-28 14:06:19 -0300

    Avance Loop

---
+++ src/app/Clases/Instruction.ts
@@ -0,0 +1,72 @@
+export class Instruction{
+
+    private id: string;
+    private type: string;
+    private destination: string;
+    private op1: string;
+    private op2: string;
+    private ciclosFor: number;
+    private UFType: string;
+    private arrayType: string;
+    private cycles: string;
+
+    constructor (id:string, type: string, destination: string, op1: string, op2: string, UFType: string, array: string){
+        this.id=id;
+        this.type=type;
+        this.destination=destination;
+        this.op1=op1;
+        this.op2=op2;
+        this.UFType = UFType; 
+        this.arrayType = array;
+    }
+
+    public setArrayType(array:string){
+        this.arrayType = array;
+    }
+
+    public getArrayType(){
+        return this.arrayType;
+    }
+
+    public setOp1(op1: string){
+        this.op1 = op1;
+    }
+
+    public setId(id :string){
+        this.id = id;
+    }
+
+    public getId(){
+        return this.id;
+    }
+
+    public getType() {
+        return this.type;
+    }
+    public getDestination() {
+        return this.destination;
+    }
+    public getOp1() {
+        return this.op1;
+    }
+    public getOp2() {
+        return this.op2;
+    }
+
+    public setciclosFor(ciclosFor) {
+        this.ciclosFor = ciclosFor;
+    }
+
+    public getciclosFor() {
+        return this.ciclosFor;
+    }
+
+    public setCycles(cycles) {
+        this.cycles = cycles;
+    }
+
+    public getcycles() {
+        return this.cycles;
+    }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,372 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Instruction } from './Clases/Instruction';
+import { toInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
+import { LoopUnrolling } from './Clases/LoopUnrolling';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TPFinalArqui2';
+
+
+export class AppComponent implements OnInit {
+
+  @ViewChild("siteConfigNetwork", { static: true }) networkContainer: ElementRef;
+
+  listInstructions = new Array<Instruction>();
+  listInstructionsUnrolling = new Array<Instruction>();
+
+  numOrder = 1;
+  numMultifunction = 0;
+  numArithmetic = 0;
+  numMemory = 0;
+  configurationSaved: boolean = false;
+  showAlert = false;
+
+  // variables simulador
+  sigInstruction = true;
+  executing = false;
+  DHeaders: Array<string>;
+  DUFHeaders: Array<string>;
+
+
+  typeInstruction = [
+    { type: "ADD", cycle: 1 },
+    { type: "SUB", cycle: 1 },
+    { type: "MUL", cycle: 1 },
+    { type: "DIV", cycle: 1 },
+    { type: "ST", cycle: 1 },
+    { type: "LD", cycle: 1 },
+    { type: "BNEZ", cycle: 1 },
+    { type: "LAZO", cycle: 1 }
+  ];
+
+  registers: string[] = [
+    "#4",
+    "#8",
+    "R0",
+    "R1",
+    "R2",
+    "R3",
+    "R4",
+    "R5",
+    "R6",
+    "R7",
+    "R8",
+    "R9",
+    "R10",
+    "R11",
+    "R12",
+    "R13",
+    "R14",
+    "R15",
+    "R16",
+    "R17",
+    "R18",
+    "R19",
+    "R20",
+    "R21",
+    "R22",
+    "R23",
+    "R24",
+    "R25",
+    "R26",
+    "R27",
+    "R28",
+    "R29",
+    "R30",
+    "R31",
+  ];
+
+  btnDefaultIns = {
+    type: "Instrucción",
+    dst: "Dst",
+    op1: "Op1",
+    op2: "Op2"
+  };
+
+  mapTipoArreglos = new Map();
+
+  btnDefaultCarga = {
+    typeReg: "Tipo",
+    cycles: "Cantidad ciclos",
+    dstLoad: "Dst",
+  };
+
+  typeArray: string[] = [
+    "ENTERO",
+    "DOUBLE",
+  ];
+
+  cycles: string[] = [
+    "100", "500", "1000", "2000", "3000", "4000"
+  ];
+
+  ngOnInit(): void {
+    const instrucions = [
+      new Instruction("", "LD", "R1", "4000", "", "ARITH", ""),
+      new Instruction("", "LAZO", "", "", "", "", ""),
+      new Instruction("", "LD", "R2", "0 (R1)", "", "MEM", "ENTERO"),
+      new Instruction("", "ADD", "R4", "R2", "R3", "ARITH", ""),
+      new Instruction("", "ST", "0 (R1)", "R4", "", "MEM", "ENTERO"),
+      new Instruction("", "SUB", "R1", "R1", "#4", "ARITH", ""),
+      new Instruction("", "BNEZ", "R1", "LAZO", "", "ARITH", "")
+    ];
+    this.listInstructions = instrucions;
+  }
+
+  changeInst(pos, name) {
+    this.btnDefaultIns[pos] = name;
+    this.updateButton();
+  }
+
+  changeCarga(pos, name) {
+    this.btnDefaultCarga[pos] = name;
+    this.updateButtonLoad();
+  }
+
+  updateButtonLoad() {
+    let btnAgregarLd = document.getElementById("btn-Agregar-LD");
+
+    if (this.btnDefaultCarga.typeReg != "Tipo" && this.btnDefaultCarga.dstLoad != "Dst" && this.btnDefaultCarga.cycles != "Cantidad ciclos")
+      btnAgregarLd.removeAttribute("disabled");
+  }
+
+  addInstructionLoad() {
+    let instNueva;
+
+    let tipo: string = this.btnDefaultCarga.typeReg;
+    let ciclos: number = parseInt(this.btnDefaultCarga.cycles);
+    let tamano: number;
+    if (tipo == "ENTERO")
+      tamano = ciclos * 4;
+    else
+      tamano = ciclos * 8;
+
+    let tamTot;
+    if (this.listInstructions.length > 0) {
+      tamTot = tamano + parseInt(this.listInstructions[this.listInstructions.length - 1].getOp1()); // Ver esto
+    } else
+      tamTot = tamano;
+
+    this.mapTipoArreglos.set(this.btnDefaultCarga.dstLoad, tipo);
+    instNueva = new Instruction("", "LD", this.btnDefaultCarga.dstLoad, tamTot.toString(), "", "ARITH", "");
+    instNueva.setciclosFor(tamano);
+    instNueva.setArrayType(tipo);
+    this.listInstructions.push(instNueva);
+  }
+
+  aux: string = "Op1";
+
+  updateButton() {
+    let btnAgr = document.getElementById("btn-Agregar");
+    let btnDst = document.getElementById("btn-dst");
+    let btnOp1 = document.getElementById("btn-op1");
+    let btnOp2 = document.getElementById("btn-op2");
+
+    switch (this.btnDefaultIns.type) {
+
+      case "ST": {
+        if (this.btnDefaultIns.op1 == "LAZO")
+          this.btnDefaultIns.op1 = this.aux;
+        btnDst.removeAttribute("disabled");
+        btnOp1.removeAttribute("disabled");
+        btnOp2.setAttribute("disabled", "");
+        btnAgr.setAttribute("disabled", "");
+        if (this.btnDefaultIns.dst != "Dst" && this.btnDefaultIns.op1 != "Op1")
+          btnAgr.removeAttribute("disabled");
+        break;
+      }
+      case "LD": {
+        if (this.btnDefaultIns.op1 == "LAZO")
+          this.btnDefaultIns.op1 = this.aux;
+        btnDst.removeAttribute("disabled");
+        btnOp1.removeAttribute("disabled");
+        btnOp2.setAttribute("disabled", "");
+        btnAgr.setAttribute("disabled", "");
+        if (this.btnDefaultIns.dst != "Dst" && this.btnDefaultIns.op1 != "Op1")
+          btnAgr.removeAttribute("disabled");
+        break;
+      }
+      case "BNEZ": {
+        if (this.btnDefaultIns.op1 == "LAZO")
+          this.btnDefaultIns.op1 = this.aux;
+        btnDst.removeAttribute("disabled");
+        btnOp1.setAttribute("disabled", "");
+        this.aux = this.btnDefaultIns.op1;
+        this.btnDefaultIns.op1 = "LAZO";
+        btnOp2.setAttribute("disabled", "");
+        if (this.btnDefaultIns.dst != "Dst")
+          btnAgr.removeAttribute("disabled");
+        break;
+      }
+      case "LAZO": {
+        if (this.btnDefaultIns.op1 == "LAZO")
+          this.btnDefaultIns.op1 = this.aux;
+        btnDst.setAttribute("disabled", "");
+        btnOp1.setAttribute("disabled", "");
+        btnOp2.setAttribute("disabled", "");
+        btnAgr.removeAttribute("disabled");
+        break;
+      }
+      default: {
+        if (this.btnDefaultIns.op1 == "LAZO")
+          this.btnDefaultIns.op1 = this.aux;
+        btnDst.removeAttribute("disabled");
+        btnOp1.removeAttribute("disabled");
+        btnOp2.removeAttribute("disabled");
+        btnAgr.setAttribute("disabled", "");
+        if (this.btnDefaultIns.type != "Instrucción" && this.btnDefaultIns.dst != "Dst" && this.btnDefaultIns.op1 != "Op1" && this.btnDefaultIns.op2 != "Op2")
+          btnAgr.removeAttribute("disabled");
+        break;
+      }
+    }
+
+  }
+
+  addInstruction() {
+    let instNueva;
+
+    if (this.btnDefaultIns.type == "ST"){
+      instNueva = new Instruction("", this.btnDefaultIns.type, "0 (" + this.btnDefaultIns.dst + ")", this.btnDefaultIns.op1, "", "MEM", this.mapTipoArreglos.get(this.btnDefaultIns.dst));
+    }
+    else
+      if (this.btnDefaultIns.type == "LD"){
+        instNueva = new Instruction("", this.btnDefaultIns.type, this.btnDefaultIns.dst, "0 (" + this.btnDefaultIns.op1 + ")", "", "MEM", this.mapTipoArreglos.get(this.btnDefaultIns.op1));
+      }
+      else
+        if (this.btnDefaultIns.type == "BNEZ")
+          instNueva = new Instruction("", this.btnDefaultIns.type, this.btnDefaultIns.dst, "LAZO", "", "ARITH", "");
+        else
+          if (this.btnDefaultIns.type == "LAZO")
+            instNueva = new Instruction("", this.btnDefaultIns.type, "", "", "", "", "");
+          else
+            instNueva = new Instruction("", this.btnDefaultIns.type, this.btnDefaultIns.dst, this.btnDefaultIns.op1, this.btnDefaultIns.op2, "ARITH", "");
+    this.listInstructions.push(instNueva);
+  }
+
+  changeOrder(num) {
+    this.numOrder = num;
+  }
+
+  deleteInstruction(inst: Instruction) {
+    let i = this.listInstructions.indexOf(inst);
+    if (this.listInstructions[i].getType() == "LD" && !this.listInstructions[i].getOp1().includes('(')) {
+      this.recalculateCycles(i, this.listInstructions[i].getciclosFor());
+      this.mapTipoArreglos.delete(this.listInstructions[i].getDestination());
+    }
+    this.listInstructions.splice(i, 1);
+
+  }
+
+  recalculateCycles(i: number, op1: number) {
+
+    for (var j = 0; j < this.listInstructions.length; j++)
+      if (this.listInstructions[j].getType() == "LD" && !this.listInstructions[j].getOp1().includes('(')) {
+        if (i < j) {
+          let actual: number = parseInt(this.listInstructions[j].getOp1());
+          this.listInstructions[j].setOp1((actual - op1).toString());
+        }
+      }
+
+  }
+
+  changeUFmultifunction(num) {
+    this.numMultifunction = num;
+  }
+
+  changeUFArithmetic(num) {
+    this.numArithmetic = num;
+  }
+
+  changeUFMemory(num) {
+    this.numMemory = num;
+  }
+
+  changeCycle(pos,numcycle){
+    for (let tipoIns of this.typeInstruction) {
+        if (tipoIns.type == pos)
+          tipoIns.cycle = numcycle;
+    }
+  }
+
+  saveConfiguration(){
+    if(this.numArithmetic!=0 || this.numMemory != 0 || this.numMultifunction!=0){
+      this.configurationSaved = true;
+      this.executing = true;
+      this.showAlert = false;
+    }
+    else{
+      this.showAlert = true;
+    }
+    this.setCycles();
+  }
+
+  private setCycles(){
+    for(let i=0 ; i< this.listInstructions.length; i++){
+      for (let j =0 ; j < this.typeInstruction.length; j++)
+        if (this.listInstructions[i].getType() == this.typeInstruction[j].type){
+          this.listInstructions[i].setCycles(this.typeInstruction[j].cycle);
+        }    
+      }   
+  }
+
+  resetConfiguration(){
+    this.configurationSaved = false;
+    this.sigInstruction = false;
+    this.executing = false;
+    this.listInstructionsUnrolling = null;
+  }
+
+  ejecutarLoopUnrolling() {
+
+    let maxUnroll = Math.max(this.numArithmetic, this.numMemory, this.numMultifunction);
+
+    let loop = new LoopUnrolling(this.listInstructions);
+    this.listInstructionsUnrolling = loop.ejecutar(maxUnroll);
+  
+    this.simuladorLoopUnrolling();
+
+  }
+
+  simuladorLoopUnrolling(){
+
+    this.sigInstruction = true;
+    this.executing = false;
+    this.createTableHead("D",this.numOrder);
+    this.createTableHeadUF("UF",this.numMultifunction,this.numMemory,this.numArithmetic);
+    
+  }
+
+  getOperandoParentesis(operando: string): string{
+    let pos1: number = operando.indexOf('(');
+    let pos2: number = operando.indexOf(')');
+    return operando.substring(pos1 + 1, pos2);
+  }
+
+  private createTableHead(desc:string, num:number){
+    let array = [];
+    for (let i = 0; i < num; i++) {
+      array.push(desc+i);
+    }
+    this[desc+'Headers'] = array;
+  }
+
+  private createTableHeadUF(desc:string, numM:number, numMem:number,numA:number){
+    let array = [];
+    for (let i = 0; i < numM; i++) {
+      array.push(desc+"M"+i);
+    }
+    for (let i = 0; i < numA; i++) {
+      array.push(desc+"A"+i);
+    }
+    for (let i = 0; i < numMem; i++) {
+      array.push(desc+"Mem"+i);
+    }    
+    this[desc+'Headers'] = array;
+  }
+
+  
+
 }
